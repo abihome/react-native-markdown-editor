8615b697236fdd0beff1242a5569cbed2bc79ee5
Lucas Rocali <rocali@outlook.com>
2024-10-03 17:42:57 -0300

    chore: update app-sample

--- app-sample/App.tsx
+++ app-sample/App.tsx
@@ -11,7 +11,7 @@ import {
   Pressable,
   ScrollView,
   InputAccessoryView,
-} from "react-native";
+} from 'react-native';
 import {
   LineKey,
   LineStyle,
@@ -20,8 +20,8 @@ import {
   TextStyleMap,
   getStyledLineMap,
   useTextEditor,
-} from "react-native-markdown-editor";
-import * as ImagePicker from "expo-image-picker";
+} from 'react-native-markdown-editor';
+import * as ImagePicker from 'expo-image-picker';
 
 type MarkdownTextProps = {
   children: string;
@@ -30,9 +30,9 @@ type MarkdownTextProps = {
 };
 
 const lineFontSizeMap: { [key in LineStyle]: number } = {
-  "#": 18,
-  "##": 16,
-  "###": 14,
+  '#': 18,
+  '##': 16,
+  '###': 14,
   body: 14,
 };
 
@@ -49,7 +49,7 @@ function MarkdownText({
     <>
       {lineKeys.map((lineKey, lineIndex) => {
         const { lineStyle, styledTexts } = styleLineMap[lineKey];
-        const lineFontWeight = lineStyle === "body" ? "400" : "600";
+        const lineFontWeight = lineStyle === 'body' ? '400' : '600';
         return (
           <Text
             key={lineKey}
@@ -63,20 +63,20 @@ function MarkdownText({
                 <Text
                   key={`t${textIndex}`}
                   style={{
-                    fontWeight: bold ? "600" : lineFontWeight,
-                    fontStyle: italic ? "italic" : "normal",
+                    fontWeight: bold ? '600' : lineFontWeight,
+                    fontStyle: italic ? 'italic' : 'normal',
                     textDecorationLine: underline
-                      ? "underline"
+                      ? 'underline'
                       : strikethrough
-                      ? "line-through"
-                      : "none",
+                      ? 'line-through'
+                      : 'none',
                   }}
                 >
                   {text}
                 </Text>
-              )
+              ),
             )}
-            {lineIndex < lineKeys.length - 1 ? "\n" : ""}
+            {lineIndex < lineKeys.length - 1 ? '\n' : ''}
           </Text>
         );
       })}
@@ -84,22 +84,22 @@ function MarkdownText({
   );
 }
 
-const LINE_TEXT_STYLES: LineStyle[] = ["#", "##", "###", "body"];
+const LINE_TEXT_STYLES: LineStyle[] = ['#', '##', '###', 'body'];
 
 const LINE_TEXT_NAME_MAP: { [key in LineStyle]: string } = {
-  "#": "Title",
-  "##": "Heading",
-  "###": "Subheading",
-  body: "Body",
+  '#': 'Title',
+  '##': 'Heading',
+  '###': 'Subheading',
+  body: 'Body',
 };
 
-const TEXT_STYLES: TextStyle[] = ["**", "__", "--", "~~"];
+const TEXT_STYLES: TextStyle[] = ['**', '__', '--', '~~'];
 
 const TEXT_STYLE_NAME_MAP: { [key in TextStyle]: string } = {
-  "**": "b",
-  __: "i",
-  "--": "u",
-  "~~": "s",
+  '**': 'b',
+  __: 'i',
+  '--': 'u',
+  '~~': 's',
 };
 
 type StyleBarProps = {
@@ -111,15 +111,15 @@ type StyleBarProps = {
 };
 
 function StyleBar({
-  currentLineStyle = "body",
+  currentLineStyle = 'body',
   currentTextStyles = [],
   onLineStyle,
   onTextStyle,
   onGetImage,
 }: StyleBarProps): JSX.Element {
   return (
-    <View style={{ backgroundColor: "#FFF" }}>
-      <View style={{ flexDirection: "row" }}>
+    <View style={{ backgroundColor: '#FFF' }}>
+      <View style={{ flexDirection: 'row' }}>
         {LINE_TEXT_STYLES.map((lineStyle) => {
           const selected = lineStyle === currentLineStyle;
           return (
@@ -127,15 +127,15 @@ function StyleBar({
               key={lineStyle}
               style={{
                 flex: 1,
-                alignItems: "center",
-                backgroundColor: selected ? "#CCC" : "#FFF",
+                alignItems: 'center',
+                backgroundColor: selected ? '#CCC' : '#FFF',
               }}
               onPress={() => onLineStyle(lineStyle)}
             >
               <Text
                 style={{
                   fontSize: lineFontSizeMap[lineStyle],
-                  fontWeight: lineStyle === "body" ? "400" : "600",
+                  fontWeight: lineStyle === 'body' ? '400' : '600',
                 }}
               >
                 {LINE_TEXT_NAME_MAP[lineStyle]}
@@ -144,7 +144,7 @@ function StyleBar({
           );
         })}
       </View>
-      <View style={{ flexDirection: "row" }}>
+      <View style={{ flexDirection: 'row' }}>
         {TEXT_STYLES.map((textStyle, i) => {
           const selected = currentTextStyles.includes(textStyle);
           return (
@@ -152,21 +152,21 @@ function StyleBar({
               key={textStyle}
               style={{
                 flex: 1,
-                alignItems: "center",
-                backgroundColor: selected ? "#CCC" : "#FFF",
+                alignItems: 'center',
+                backgroundColor: selected ? '#CCC' : '#FFF',
               }}
               onPress={() => onTextStyle(textStyle)}
             >
               <Text
                 style={{
-                  fontWeight: textStyle === "**" ? "600" : "400",
-                  fontStyle: textStyle === "__" ? "italic" : "normal",
+                  fontWeight: textStyle === '**' ? '600' : '400',
+                  fontStyle: textStyle === '__' ? 'italic' : 'normal',
                   textDecorationLine:
-                    textStyle === "--"
-                      ? "underline"
-                      : textStyle === "~~"
-                      ? "line-through"
-                      : "none",
+                    textStyle === '--'
+                      ? 'underline'
+                      : textStyle === '~~'
+                      ? 'line-through'
+                      : 'none',
                 }}
               >
                 {TEXT_STYLE_NAME_MAP[textStyle]}
@@ -178,11 +178,11 @@ function StyleBar({
       <Pressable
         style={{
           flex: 1,
-          alignItems: "center",
+          alignItems: 'center',
         }}
         onPress={() => onGetImage()}
       >
-        <Text>{"Add Photo"}</Text>
+        <Text>{'Add Photo'}</Text>
       </Pressable>
     </View>
   );
@@ -190,7 +190,7 @@ function StyleBar({
 
 export default function App() {
   const initialMarkdownText =
-    "#Title example\n\n##Heading example\n\n###Subheading example\n\nContent example, testing **bold**, __italic__ --underline-- and ~~strikethrough~~\n";
+    '#Title example\n\n##Heading example\n\n###Subheading example\n\nContent example, testing **bold**, __italic__ --underline-- and ~~strikethrough~~\n';
   const {
     inputBlockInfoMap,
     markdown,
@@ -207,7 +207,7 @@ export default function App() {
   });
 
   const inputBlockKeys = Object.keys(inputBlockInfoMap) as LineKey[];
-  const inputAccessoryViewID = "toolbarAcessoryView";
+  const inputAccessoryViewID = 'toolbarAcessoryView';
 
   const rawTextBlocks = markdown.split(/<img>|<\/img>/);
 
@@ -220,7 +220,7 @@ export default function App() {
         <View style={{ minHeight: 200 }}>
           {inputBlockKeys.map((inputBlockKey, i) => {
             const inputBlockInfo = inputBlockInfoMap[inputBlockKey];
-            if (inputBlockInfo.type === "text") {
+            if (inputBlockInfo.type === 'text') {
               const { text, textStyleMap, lineStyleMap } = inputBlockInfo;
               return (
                 <TextInput
@@ -233,7 +233,7 @@ export default function App() {
                   }
                   onEndEditing={() => {
                     if (markdown != initialMarkdownText) {
-                      console.log("onSave", markdown);
+                      console.log('onSave', markdown);
                     }
                   }}
                   onSelectionChange={({
@@ -256,19 +256,19 @@ export default function App() {
                 <View>
                   <Image
                     source={{ uri: `data:image/png;base64,${imgUrl}` }}
-                    resizeMode={"cover"}
-                    style={{ width: "100%", height: 200 }}
+                    resizeMode={'cover'}
+                    style={{ width: '100%', height: 200 }}
                   />
                   <Pressable
                     style={{
-                      position: "absolute",
+                      position: 'absolute',
                       top: 0,
                       right: 0,
-                      backgroundColor: "#FFF",
+                      backgroundColor: '#FFF',
                     }}
                     onPress={() => onRemovePhoto(inputBlockKey)}
                   >
-                    <Text>{"Remove Photo"}</Text>
+                    <Text>{'Remove Photo'}</Text>
                   </Pressable>
                 </View>
               );
@@ -278,13 +278,13 @@ export default function App() {
         {rawTextBlocks.map((rawTextBlock, i) => {
           if (i % 2 == 1) {
             return (
-              <Text style={{ borderColor: "#999", borderWidth: 1 }}>
+              <Text style={{ borderColor: '#999', borderWidth: 1 }}>
                 {`data:image/png;base64,${rawTextBlock.substring(0, 40)}...`}
               </Text>
             );
           }
           return (
-            <Text style={{ borderColor: "#999", borderWidth: 1 }}>
+            <Text style={{ borderColor: '#999', borderWidth: 1 }}>
               {rawTextBlock}
             </Text>
           );
@@ -319,7 +319,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     padding: 16,
   },
 });
